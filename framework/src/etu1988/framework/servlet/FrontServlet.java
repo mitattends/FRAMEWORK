52a121b82e3e5e569c1a8b34a97ca8b1461c3ed2
mitattends <mitantsoarajaonera@gmail.com>
2023-06-06 09:42:02 +0300

    array in html

--- framework/src/etu1988/framework/servlet/FrontServlet.java
+++ framework/src/etu1988/framework/servlet/FrontServlet.java
@@ -27,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Date;
 import java.util.Collections;
+import java.util.Vector;
 
 
 /**
